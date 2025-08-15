bdff50b1abe0de9ba3191fb489f9656b549e8ed4
Nyasa Dwivedi <168264046+nyasa10@users.noreply.github.com>
2025-08-15 21:39:35 +0530

    SQL Queries

---
+++ data cleaning.sql
@@ -0,0 +1,60 @@
+CREATE DATABASE projects;
+USE projects;
+SELECT * FROM hr;
+ALTER TABLE hr
+CHANGE COLUMN ï»¿id emp_id VARCHAR(20) NULL;
+
+DESCRIBE hr;
+SELECT birthdate FROM hr;
+
+SET sql_safe_updates = 0;
+
+UPDATE hr
+SET birthdate = CASE 
+WHEN birthdate LIKE '%/%' THEN date_format(str_to_date(birthdate, '%m/%d/%Y'), '%Y-%m-%d')
+WHEN birthdate LIKE '%-%' THEN date_format(str_to_date(birthdate, '%m-%d-%Y'), '%Y-%m-%d')
+ELSE NULL 
+END;
+SELECT birthdate FROM hr;
+
+ALTER TABLE hr
+MODIFY COLUMN birthdate DATE;
+
+UPDATE hr
+SET hire_date = CASE 
+WHEN hire_date LIKE '%/%' THEN date_format(str_to_date(hire_date, '%m/%d/%Y'), '%Y-%m-%d')
+WHEN hire_date LIKE '%-%' THEN date_format(str_to_date(hire_date, '%m-%d-%Y'), '%Y-%m-%d')
+ELSE NULL 
+END;
+SELECT hire_date FROM hr;
+
+ALTER TABLE hr
+MODIFY COLUMN hire_date DATE;
+
+UPDATE hr
+SET termdate = IF(termdate IS NOT NULL AND termdate != '', date(str_to_date(termdate, '%Y-%m-%d %H:%i:%s UTC')), '0000-00-00')
+WHERE true;
+
+SELECT termdate from hr;
+
+SET sql_mode = 'ALLOW_INVALID_DATES';
+
+ALTER TABLE hr
+MODIFY COLUMN termdate DATE;
+
+ALTER TABLE hr ADD COLUMN age INT; 
+
+UPDATE hr
+SET age = timestampdiff(YEAR, birthdate, CURDATE());
+SELECT age FROM hr;
+
+SELECT 
+min(age) AS youngest,
+max(age) AS oldest
+FROM hr;
+
+SELECT count(*) FROM hr WHERE age < 18;
+
+UPDATE hr
+SET birthdate = DATE_SUB(birthdate, INTERVAL 100 YEAR)
+WHERE birthdate >= '2060-01-01' AND birthdate < '2070-01-01';
